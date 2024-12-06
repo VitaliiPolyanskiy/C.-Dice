d27a2bbb900ce1386a390fe265867417864a4356
Vitaliy <vitaliypolyanskiy@gmail.com>
2024-12-06 21:48:05 +0200

    Add project files.

---
+++ dice/Source.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\n\t" << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187);
+	cout << "\n\t" << char(186) << "       " << char(186);
+	cout << "\n\t" << char(186) << "   *   " << char(186);
+	cout << "\n\t" << char(186) << "       " << char(186);
+	cout << "\n\t" << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(188);
+	cout << endl << endl;
+	cout << "\n\t" << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187);
+	cout << "\n\t" << char(186) << "     * " << char(186);
+	cout << "\n\t" << char(186) << "       " << char(186);
+	cout << "\n\t" << char(186) << " *     " << char(186);
+	cout << "\n\t" << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(188);
+	cout << endl << endl;
+	cout << "\n\t" << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187);
+	cout << "\n\t" << char(186) << "     * " << char(186);
+	cout << "\n\t" << char(186) << "   *   " << char(186);
+	cout << "\n\t" << char(186) << " *     " << char(186);
+	cout << "\n\t" << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(188);
+	cout << endl << endl;
+	cout << "\n\t" << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187);
+	cout << "\n\t" << char(186) << " *   * " << char(186);
+	cout << "\n\t" << char(186) << "       " << char(186);
+	cout << "\n\t" << char(186) << " *   * " << char(186);
+	cout << "\n\t" << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(188);
+	cout << endl << endl;
+	cout << "\n\t" << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187);
+	cout << "\n\t" << char(186) << " *   * " << char(186);
+	cout << "\n\t" << char(186) << "   *   " << char(186);
+	cout << "\n\t" << char(186) << " *   * " << char(186);
+	cout << "\n\t" << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(188);
+	cout << endl << endl;
+	cout << "\n\t" << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187);
+	cout << "\n\t" << char(186) << " *   * " << char(186);
+	cout << "\n\t" << char(186) << " *   * " << char(186);
+	cout << "\n\t" << char(186) << " *   * " << char(186);
+	cout << "\n\t" << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(188);
+
+
+	return 0;
+}
